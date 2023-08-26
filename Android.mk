9532b9d333b416bf8d02c39dec7974e294dcf44f
snnbyyds <snnbyyds@gmail.com>
2023-08-26 12:52:41 +0800

    lahaina: Fixup!

--- Android.mk
+++ Android.mk
@@ -5,12 +5,12 @@ include $(CLEAR_VARS)
     LOCAL_MODULE_CLASS := ETC
     LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)
     LOCAL_POST_INSTALL_CMD += \
-        mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules; \
-        cp -rf $(LOCAL_PATH)/modules/adsp_loader_dlkm.ko $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/; \
-        cp -rf $(LOCAL_PATH)/modules/apr_dlkm.ko $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/; \
-        cp -rf $(LOCAL_PATH)/modules/msm_drm.ko $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/; \
-        cp -rf $(LOCAL_PATH)/modules/q6_dlkm.ko $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/; \
-        cp -rf $(LOCAL_PATH)/modules/q6_notifier_dlkm.ko $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/; \
-        cp -rf $(LOCAL_PATH)/modules/q6_pdr_dlkm.ko $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/; \
+        mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/1.1; \
+        cp -rf $(LOCAL_PATH)/modules/1.1/adsp_loader_dlkm.ko $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/1.1/; \
+        cp -rf $(LOCAL_PATH)/modules/1.1/apr_dlkm.ko $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/1.1/; \
+        cp -rf $(LOCAL_PATH)/modules/1.1/msm_drm.ko $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/1.1/; \
+        cp -rf $(LOCAL_PATH)/modules/1.1/q6_dlkm.ko $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/1.1/; \
+        cp -rf $(LOCAL_PATH)/modules/1.1/q6_notifier_dlkm.ko $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/1.1/; \
+        cp -rf $(LOCAL_PATH)/modules/1.1/q6_pdr_dlkm.ko $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/1.1/; \
         cp -rf $(LOCAL_PATH)/modules.load.recovery $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/1.1/;
 include $(BUILD_PHONY_PACKAGE)
